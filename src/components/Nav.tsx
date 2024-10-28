96e66fe47f0d33a653ca05c175dc6bd17f7f447d
Mohd Dawood <serious17org@gmail.com>
2024-10-28 17:05:55 +0530

    2. Made Our Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,27 @@
 import logo from "../assets/logo.svg";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import style from "../styles/Nav.module.scss";
 
 const Nav = () => {
   return (
     <nav>
-      <div className="logo">
+      <div className={style.logo}>
         <h1>Foodie Crush</h1>
         <img src={logo} alt="logo" />
       </div>
 
-      <form className="search">
-        <input type="text" name="search-input" />
+      <form className={style.searchBar}>
+        <input
+          placeholder="search for a recipe"
+          type="text"
+          name="search-input"
+          required
+        />
         <button>
           <Icon
             icon="fluent:search-48-filled"
-            width="16px"
-            height="16px"
+            width="24px"
+            height="24px"
             style={{ color: "white" }}
           />
         </button>
